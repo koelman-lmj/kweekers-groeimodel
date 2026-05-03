131db8d1ffe8e370ab3b4bd1abe0cf7e790031b5
koelman-lmj <s.koelman@kweekers.nl>
2026-05-03 09:28:15 +0200

    Delete app/context/app/scan/[id]/layout.tsx

--- app/context/app/scan/[id]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "react";
-
-export default function ScanLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  return <main className="mx-auto max-w-4xl p-8">{children}</main>;
-}
