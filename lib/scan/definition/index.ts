38d8b97ab39a46d988afeafb65fc4a4312592f51
koelman-lmj <s.koelman@kweekers.nl>
2026-05-06 23:01:35 +0200

    Update index.ts

--- lib/scan/definition/index.ts
+++ lib/scan/definition/index.ts
@@ -1,12 +1,10 @@
 import type { ScanDefinition } from "../types";
+import { sections } from "./sections";
 import { optionSets } from "./option-sets";
 import { questions } from "./questions";
-import { sections } from "./sections";
 
 export const scanDefinition: ScanDefinition = {
-  version: "0.1.0",
   sections,
   optionSets,
   questions,
-  rules: [],
 };
