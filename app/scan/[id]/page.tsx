e08b0526422c49960425465159a5d7cab5f82cd2
koelman-lmj <s.koelman@kweekers.nl>
2026-05-09 00:09:44 +0200

    Update page.tsx

--- app/scan/[id]/page.tsx
+++ app/scan/[id]/page.tsx
@@ -1,1 +1,10 @@
+import { redirect } from "next/navigation";
 
+type PageProps = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function ScanIdPage({ params }: PageProps) {
+  const { id } = await params;
+  redirect(`/scan/${id}/summary/advies`);
+}
