584ae3129a223655a62867aff486788ffa9fb842
koelman-lmj <s.koelman@kweekers.nl>
2026-05-14 14:45:20 +0200

    Update index.ts

--- lib/scan/definition/index.ts
+++ lib/scan/definition/index.ts
@@ -1,10 +1,27 @@
-import type { ScanDefinition } from "../types";
 import { sections } from "./sections";
 import { optionSets } from "./option-sets";
 import { questions } from "./questions";
+import { dimensions } from "./dimensions";
 
-export const scanDefinition: ScanDefinition = {
+export const scanDefinition = {
   sections,
   optionSets,
   questions,
+  dimensions,
 };
+
+export { sections } from "./sections";
+export { optionSets } from "./option-sets";
+export { questions } from "./questions";
+export { dimensions } from "./dimensions";
+
+export type {
+  DimensionCategory,
+  OutputRole,
+  QuestionInputType,
+  VisibleWhenCondition,
+  QuestionDefinition,
+  OptionDefinition,
+  OptionSetDefinition,
+  SectionDefinition,
+} from "./types";
